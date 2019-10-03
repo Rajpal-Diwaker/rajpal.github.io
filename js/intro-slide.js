7363d8a245c48672beebf63547d5d50e258f11b1
Ankit verma <ankitvermamat@gmail.com>
2019-10-03 23:04:55 +0530

    proj-btn

--- js/intro-slide.js
+++ js/intro-slide.js
@@ -1,10 +1,14 @@
-window.addEventListener("load", () => {
+var introTextSliderId = setInterval(() => {
+  const textSlides = 3;
   const slider = document.getElementById("text-slider");
+  if (!slider) return;
   const slides = slider.children;
-  let slide = 0;
+  if (slides.length !== textSlides) return;
+  let currentSlide = 0;
   setInterval(() => {
     for (let i = 0; i < slides.length; i++) slides[i].style.display = "none";
-    slides[slide].style.display = "block";
-    slide = (slide + 1) % 3;
+    slides[currentSlide].style.display = "block";
+    currentSlide = (currentSlide + 1) % 3;
   }, 2500);
-});
+  clearInterval(introTextSliderId);
+}, 100);
