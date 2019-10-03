40877d313139c277da336fb9f96d3f3c3943494a
Ankit verma <ankitvermamat@gmail.com>
2019-10-03 21:02:58 +0530

    added projects and modified table

--- js/achievement-table.js
+++ js/achievement-table.js
@@ -96,92 +96,120 @@
 
 // })();
 
-(function () {
-    console.log("jnvk")
-    var data = [
-        {
-            event: 'TCS Codevita VII Round 1',
-            org: 'TCS Global',
-            indRank: '45/43000 ',
-            globalRank:'45/43000',
-            Date:'Aug 2018 ',
-        },
-        {
-            event: 'Zomato Hackathon',
-            org: 'Zomato',
-            indRank: '37',
-            globalRank:'37',
-            Date:'Dec 2018 ',
-        },
-        {
-            event: 'TCS Codevita VII Round 2',
-            org: 'TCS Global',
-            indRank: '17',
-            globalRank:'36',
-            Date:'Dec 2018 ',
-        },
-        {
-            event: 'Interviewbit Hiring Challenge',
-            org: 'Interviewbit',
-            indRank: '24',
-            globalRank:'24',
-            Date:'Dec 2018 ',
-        },
-        {
-            event: 'April Long Challenge',
-            org: 'Codechef',
-            indRank: '--',
-            globalRank:'155/7809',
-            Date:'Apr 2018 ',
-        },
-        {
-            event: 'January Cook Off',
-            org: 'Codechef',
-            indRank: '--',
-            globalRank:'123/1738',
-            Date:'Apr 2018 ',
-        },
-        {
-            event: 'Hourrank 29',
-            org: 'Hackerrank',
-            indRank: '--',
-            globalRank:'141/1906',
-            Date:'Aug 2018 ',
-        },
-        {
-            event: 'Code-saga (Online coding contest)',
-            org: 'IOSD',
-            indRank: '1',
-            globalRank:'1',
-            Date:'Apr 2018 ',
-        },
-        {
-            event: 'AlgoJam (Coding competition)',
-            org: 'MSIT - Avensis',
-            indRank: '1',
-            globalRank:'1',
-            Date:'Jan 2018 ',
-        },
-        {
-            event: 'Codesense(Team Coding competition)',
-            org: 'IEEE - MAIT',
-            indRank: '1',
-            globalRank:'1',
-            Date:'Feb 2018 ',
-        },
-    ]
+(function() {
+  var data = [
+    {
+      event: "Kotlin Heroes",
+      org: "Jetbrains",
+      indRank: "8/700",
+      glbRank: "225/4560",
+      date: "Jun 2019",
+      link:"https://codeforces.com/contest/1170"
+    },
+    {
+      event: "TCS Codevita 8 Round 1",
+      org: "TCS Global",
+      indRank: "6th rank",
+      glbRank: "---",
+      date: "Aug 2019"
+    },
+    {
+      event: "TCS Codevita VII Round 1",
+      org: "TCS Global",
+      indRank: "45/43000 ",
+      glbRank: "---",
+      date: "Aug 2018 "
+    },
+    {
+      event: "Zomato Hackathon",
+      org: "Zomato",
+      indRank: "37",
+      glbRank: "37",
+      date: "Dec 2018 ",
+      link: "https://www.interviewbit.com/contest/hackanoodle-coding-for-scale/"
+    },
+    {
+      event: "TCS Codevita VII Round 2",
+      org: "TCS Global",
+      indRank: "17",
+      glbRank: "36",
+      date: "Dec 2018 "
+    },
+    {
+      event: "Interviewbit Hiring Challenge",
+      org: "Interviewbit",
+      indRank: "24",
+      glbRank: "24",
+      date: "Dec 2018 "
+    },
+    {
+      event: "April Long Challenge Div 1",
+      org: "Codechef",
+      indRank: "77",
+      glbRank: "155/7809",
+      date: "Apr 2018 ",
+      link: "http://www.codechef.com/APRIL18A"
+    },
+    {
+      event: "January Cook Off",
+      org: "Codechef",
+      indRank: "62",
+      glbRank: "123/1738",
+      date: "Apr 2018 ",
+      link: "http://www.codechef.com/COOK90"
+    },
+    {
+      event: "Hourrank 29",
+      org: "Hackerrank",
+      indRank: "45",
+      glbRank: "141/1906",
+      date: "Aug 2018 ",
+      link: "https://www.hackerrank.com/contests/hourrank-29/challenges"
+    },
+    {
+      event: "Code-saga (Online coding contest)",
+      org: "IOSD",
+      indRank: "1",
+      glbRank: "1",
+      date: "Apr 2018 "
+    },
+    {
+      event: "AlgoJam (Coding competition)",
+      org: "MSIT - Avensis",
+      indRank: "1",
+      glbRank: "1",
+      date: "Jan 2018 "
+    },
+    {
+      event: "Codesense(Team Coding competition)",
+      org: "IEEE - MAIT",
+      indRank: "1",
+      glbRank: "1",
+      date: "Feb 2018 "
+    }
+  ];
 
-    var tbody = document.getElementById('achv-main-table').getElementsByTagName('tbody')
-    console.log(432,tbody)
-    for (let i = 0; i < data.length; i++) {
-        $(tbody).append(`
+  var tbody = document
+    .getElementById("achv-main-table")
+    .getElementsByTagName("tbody");
+  console.log(432, tbody);
+  for (let i = 0; i < data.length; i++) {
+    $(tbody).append(`
         <tr>
-        <td>${data[i].event}</td>
+        ${
+          data[i].link
+            ? `<td class="td-link-row">${data[i].event}
+                    <a href="${data[i].link}" target="_blank">
+                        <img width="16" height="16" src="https://img.icons8.com/dotty/80/000000/external-link.png">
+                    </a>
+                </td>`
+            : `<td>${data[i].event}</td>`
+        }
         <td>${data[i].org}</td>
         <td>${data[i].indRank}</td>
-        <td>${data[i].globalRank}</td>
-        <td>${data[i].Date}</td>
+        <td>${data[i].glbRank}</td>
+        <td>${data[i].date}</td>
         </tr>
-    `)
-    }
+    `);
+  }
 })();
