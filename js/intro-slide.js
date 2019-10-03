a8ec5eee89292c457457a44c45fc285b213c43f1
Ankit verma <ankitvermamat@gmail.com>
2019-10-04 02:11:27 +0530

    fixed size for intro-bkg

--- js/intro-slide.js
+++ js/intro-slide.js
@@ -1,14 +1,21 @@
-var introTextSliderId = setInterval(() => {
-  const textSlides = 3;
-  const slider = document.getElementById("text-slider");
-  if (!slider) return;
-  const slides = slider.children;
-  if (slides.length !== textSlides) return;
-  let currentSlide = 0;
-  setInterval(() => {
-    for (let i = 0; i < slides.length; i++) slides[i].style.display = "none";
-    slides[currentSlide].style.display = "block";
-    currentSlide = (currentSlide + 1) % 3;
-  }, 2500);
-  clearInterval(introTextSliderId);
-}, 100);
+function startSlides() {
+    const textSlides = 3,
+      slider = document.getElementById("text-slider"),
+      introHead = document.getElementById("intro-head");
+
+    const slides = slider.children;
+
+    let currentSlide = 1;
+    introHead.style.display = "none";
+    slider.style.display = "block";
+
+    setInterval(() => {
+      for (let i = 0; i < slides.length; i++) slides[i].style.display = "none";
+      slides[currentSlide].style.display = "block";
+      currentSlide = (currentSlide + 1) % 3;
+    }, 3000);
+}
+
+window.addEventListener("load", () => {
+  setTimeout(startSlides, 1000);
+});
